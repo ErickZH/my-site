5e55895f826f08c669be8711cc2038dc1915f15b
ErickZH <zaraterick@outlook.com>
2019-10-30 00:35:43 -0600

    Add component header

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default() => (
+    <header className="bg-blue-600">
+        <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
+            <div className="flex-1">
+                <h1>¡Hola! Soy Erick</h1>
+                <p>Creo aplicaciones web con Laravel y Vue js.</p>
+            </div>
+        </div>
+    </header>
+)
