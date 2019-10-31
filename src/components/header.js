633f9debbc8d7b2be5689d8fb3f293088bb53108
ErickZH <zaraterick@outlook.com>
2019-10-30 22:03:10 -0600

    Add image

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
 import React from "react"
+import ilustration from '../images/developer.svg'
 
 export default() => (
-    <header className="bg-blue-600">
+    <header className="bg-gray-300">
         <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
             <div className="flex-1">
-                <h1>¡Hola! Soy Erick</h1>
-                <p>Creo aplicaciones web con Laravel y Vue js.</p>
+                <h1 className="font-bold text-purple-700 text-6xl">¡Hola! Soy Erick</h1>
+                <p className="text-md font-light">Creo aplicaciones web con Laravel y Vue js.</p>
             </div>
+            <img src={ilustration} alt="Work in progress" style={{height:"300px"}}></img>
         </div>
     </header>
 )
