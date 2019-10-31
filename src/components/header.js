fe7f37059ec34036ba75cbe3fabdcd05a2628124
ErickZH <zaraterick@outlook.com>
2019-10-30 22:59:22 -0600

    Add section contact

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,21 @@
 import React from "react"
 import ilustration from '../images/developer.svg'
+import Form from './contact'
 
 export default() => (
     <header className="bg-gray-300">
-        <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
-            <div className="flex-1">
-                <h1 className="font-bold text-purple-700 text-6xl">¡Hola! Soy Erick</h1>
-                <p className="text-md font-light">Creo aplicaciones web con Laravel y Vue js.</p>
+        <div className="container mx-auto p-12 max-w-4xl">
+            <div className="flex justify-center items-center">
+                <div className="flex-1">
+                    <h1 className="font-bold text-purple-700 text-6xl">¡Hola! Soy Erick</h1>
+                    <p className="text-md font-light">Creo aplicaciones web con Laravel y Vue js.</p>
+                </div>
+                <img src={ilustration} alt="Work in progress" style={{height:"300px"}}></img>
             </div>
-            <img src={ilustration} alt="Work in progress" style={{height:"300px"}}></img>
+
+            <div>
+                <Form/>
+            </div>
         </div>
     </header>
 )
